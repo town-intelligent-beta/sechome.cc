ef7389f1e970af8d3c45983a5ab319f4ab513093
HuangJyunYu <yillkid@gmail.com>
2024-11-04 20:55:50 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,13 +1,13 @@
 // Host
-HOST_URL_EID_DAEMON = "https://beta-eid-backend.4impact.cc";
-HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.4impact.cc";
-HOST_URL_LLMTWINS = "https://beta-llmtwins.4impact.cc"
+HOST_URL_EID_DAEMON = "https://rc-eid-backend.4impact.cc";
+HOST_URL_TPLANET_DAEMON = "https://rc-tplanet-backend.4impact.cc";
+HOST_URL_LLMTWINS = "https://llmtwins.4impact.cc"
 
 // EID
-HOST_URL_EID = "https://beta-eid.townway.com.tw";
+HOST_URL_EID = "https://eid.townway.com.tw";
 
 // Site hoster
-SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org", "400@gmail.com"]
+SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org"]
 
 // Site information
 SITE_TYPE = 0 // 0 or 1(公板)
@@ -16,7 +16,6 @@ COPYRIGHT = "Second Home"
 
 // Weight : SDGs, 人文地產景, 德智體群美
 WEIGHT = [1, 1, 1]
-WEIGHTS = ["SDGs", "CommunityDevelopment", "FiveWaysofLife"]
 
 // FEATURES
 LLM = true
